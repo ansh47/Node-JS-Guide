05d87e1b6693c3a9e58803acf07a687644781342
Ankit Shrivastava <ankit.sim1693@gmail.com>
2021-12-08 18:35:06 +0530

    pug done, handlebars started

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
+const bodyParser = require("body-parser");
+
+const expressHbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
-app.set("view engine", "pug");
+
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
 app.set("views", "views");
+
+// app.set("view engine", "pug");
+// app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,7 +24,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
