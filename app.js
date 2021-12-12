78296d781dbde2f6b3c07bc825c44e90f59fb959
Ankit Shrivastava <ankit.sim1693@gmail.com>
2021-12-12 21:20:29 +0530

    DB updating and deletion!

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -15,11 +15,18 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
+
