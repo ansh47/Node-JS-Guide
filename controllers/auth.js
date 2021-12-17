4939fc16fbfad8c9327110a2d828833e82a7e2d7
Ankit Shrivastava <ankit.sim1693@gmail.com>
2021-12-17 18:21:18 +0530

    Mobile view customization!

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,17 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn =
-    req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
     isAuthenticated: false,
   });
 };
@@ -22,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
